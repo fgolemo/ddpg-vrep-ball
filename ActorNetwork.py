44448c2d85e467682f21b4b68a20b4105a0b1a5e
Florian Golemo <fgolemo@gmail.com>
2017-05-27 18:20:49 +0200

    init with working model

--- ActorNetwork.py
+++ ActorNetwork.py
@@ -1,16 +1,11 @@
-import numpy as np
-import math
-from keras.initializations import normal, identity
-from keras.models import model_from_json
-from keras.models import Sequential, Model
-from keras.engine.training import collect_trainable_weights
-from keras.layers import Dense, Flatten, Input, merge, Lambda
-from keras.optimizers import Adam
-import tensorflow as tf
 import keras.backend as K
+import tensorflow as tf
+from keras.initializers import RandomNormal
+from keras.layers import Dense, Input, merge, Lambda
+from keras.models import Model
 
-HIDDEN1_UNITS = 300
-HIDDEN2_UNITS = 600
+HIDDEN1_UNITS = 100
+HIDDEN2_UNITS = 200
 
 class ActorNetwork(object):
     def __init__(self, sess, state_size, action_size, BATCH_SIZE, TAU, LEARNING_RATE):
@@ -21,10 +16,10 @@ class ActorNetwork(object):
 
         K.set_session(sess)
 
-        #Now create the model
-        self.model , self.weights, self.state = self.create_actor_network(state_size, action_size)   
-        self.target_model, self.target_weights, self.target_state = self.create_actor_network(state_size, action_size) 
-        self.action_gradient = tf.placeholder(tf.float32,[None, action_size])
+        # Now create the model
+        self.model, self.weights, self.state = self.create_actor_network(state_size, action_size)
+        self.target_model, self.target_weights, self.target_state = self.create_actor_network(state_size, action_size)
+        self.action_gradient = tf.placeholder(tf.float32, [None, action_size])
         self.params_grad = tf.gradients(self.model.output, self.weights, -self.action_gradient)
         grads = zip(self.params_grad, self.weights)
         self.optimize = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(grads)
@@ -40,18 +35,16 @@ class ActorNetwork(object):
         actor_weights = self.model.get_weights()
         actor_target_weights = self.target_model.get_weights()
         for i in xrange(len(actor_weights)):
-            actor_target_weights[i] = self.TAU * actor_weights[i] + (1 - self.TAU)* actor_target_weights[i]
+            actor_target_weights[i] = self.TAU * actor_weights[i] + (1 - self.TAU) * actor_target_weights[i]
         self.target_model.set_weights(actor_target_weights)
 
-    def create_actor_network(self, state_size,action_dim):
+    def create_actor_network(self, state_size, action_dim):
         print("Now we build the model")
-        S = Input(shape=[state_size])   
+        S = Input(shape=[state_size])
         h0 = Dense(HIDDEN1_UNITS, activation='relu')(S)
         h1 = Dense(HIDDEN2_UNITS, activation='relu')(h0)
-        Steering = Dense(1,activation='tanh',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1)  
-        Acceleration = Dense(1,activation='sigmoid',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1)   
-        Brake = Dense(1,activation='sigmoid',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1) 
-        V = merge([Steering,Acceleration,Brake],mode='concat')          
-        model = Model(input=S,output=V)
+        Steering = Dense(1, activation='tanh', kernel_initializer='random_normal')(h1)
+        Steering = Lambda(lambda x: x * 2)(Steering)  # scaling up to -2,2
+        # V = merge([Steering], mode='concat')
+        model = Model(input=S, output=Steering)
         return model, model.trainable_weights, S
-
--- ddpg.py
+++ ddpg.py
@@ -1,112 +1,90 @@
-from gym_torcs import TorcsEnv
-import numpy as np
-import random
-import argparse
-from keras.models import model_from_json, Model
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.optimizers import Adam
-import tensorflow as tf
-from keras.engine.training import collect_trainable_weights
 import json
+import numpy as np
 
-from ReplayBuffer import ReplayBuffer
+import gym
+import tensorflow as tf
+
 from ActorNetwork import ActorNetwork
 from CriticNetwork import CriticNetwork
 from OU import OU
-import timeit
+from ReplayBuffer import ReplayBuffer
 
-OU = OU()       #Ornstein-Uhlenbeck Process
+OU = OU()  # Ornstein-Uhlenbeck Process
 
-def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
-    BUFFER_SIZE = 100000
+
+def playGame(train_indicator=1, render=True, debug=True):  # 1 means Train, 0 means simply Run
+    if train_indicator == 1:
+        log = open("./rewards.log", 'w')
+
+    BUFFER_SIZE = 10000
     BATCH_SIZE = 32
     GAMMA = 0.99
-    TAU = 0.001     #Target Network HyperParameters
-    LRA = 0.0001    #Learning rate for Actor
-    LRC = 0.001     #Lerning rate for Critic
+    TAU = 0.001  # Target Network HyperParameters
+    LRA = 0.0001  # Learning rate for Actor
+    LRC = 0.001  # Lerning rate for Critic
 
-    action_dim = 3  #Steering/Acceleration/Brake
-    state_dim = 29  #of sensors input
+    action_dim = 1  # Steering/Acceleration/Brake
+    state_dim = 3  # of sensors input
 
     np.random.seed(1337)
 
-    vision = False
-
-    EXPLORE = 100000.
-    episode_count = 2000
-    max_steps = 100000
-    reward = 0
-    done = False
+    EXPLORE = 10000.
+    episode_count = 200
+    max_steps = 1000
     step = 0
     epsilon = 1
-    indicator = 0
 
-    #Tensorflow GPU optimization
     config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
     from keras import backend as K
     K.set_session(sess)
 
     actor = ActorNetwork(sess, state_dim, action_dim, BATCH_SIZE, TAU, LRA)
     critic = CriticNetwork(sess, state_dim, action_dim, BATCH_SIZE, TAU, LRC)
-    buff = ReplayBuffer(BUFFER_SIZE)    #Create replay buffer
+    buff = ReplayBuffer(BUFFER_SIZE)  # Create replay buffer
 
-    # Generate a Torcs environment
-    env = TorcsEnv(vision=vision, throttle=True,gear_change=False)
+    env = gym.make('Pendulum-v0')
 
-    #Now load the weight
-    print("Now we load the weight")
-    try:
-        actor.model.load_weights("actormodel.h5")
-        critic.model.load_weights("criticmodel.h5")
-        actor.target_model.load_weights("actormodel.h5")
-        critic.target_model.load_weights("criticmodel.h5")
-        print("Weight load successfully")
-    except:
-        print("Cannot find the weight")
+    if train_indicator == 0:
+        try:
+            actor.model.load_weights("actormodel.h5")
+            critic.model.load_weights("criticmodel.h5")
+            actor.target_model.load_weights("actormodel.h5")
+            critic.target_model.load_weights("criticmodel.h5")
+            print("Weight load successfully")
+        except:
+            print("Cannot find the weight")
 
     print("TORCS Experiment Start.")
     for i in range(episode_count):
 
         print("Episode : " + str(i) + " Replay Buffer " + str(buff.count()))
 
-        if np.mod(i, 3) == 0:
-            ob = env.reset(relaunch=True)   #relaunch TORCS every 3 episode because of the memory leak error
-        else:
-            ob = env.reset()
+        ob = env.reset()
 
-        s_t = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY,  ob.speedZ, ob.wheelSpinVel/100.0, ob.rpm))
-     
+        s_t = ob
+
         total_reward = 0.
         for j in range(max_steps):
-            loss = 0 
+            loss = 0
             epsilon -= 1.0 / EXPLORE
-            a_t = np.zeros([1,action_dim])
-            noise_t = np.zeros([1,action_dim])
-            
-            a_t_original = actor.model.predict(s_t.reshape(1, s_t.shape[0]))
-            noise_t[0][0] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][0],  0.0 , 0.60, 0.30)
-            noise_t[0][1] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][1],  0.5 , 1.00, 0.10)
-            noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2], -0.1 , 1.00, 0.05)
+            a_t = np.zeros([1, action_dim])
+            noise_t = np.zeros([1, action_dim])
 
-            #The following code do the stochastic brake
-            #if random.random() <= 0.1:
-            #    print("********Now we apply the brake***********")
-            #    noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2],  0.2 , 1.00, 0.10)
+            a_t_original = actor.model.predict(s_t.reshape(1, s_t.shape[0]))
+            noise_t[0][0] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][0], 0.0, 0.60, 0.30)
 
             a_t[0][0] = a_t_original[0][0] + noise_t[0][0]
-            a_t[0][1] = a_t_original[0][1] + noise_t[0][1]
-            a_t[0][2] = a_t_original[0][2] + noise_t[0][2]
 
+            if render:
+                env.render()
             ob, r_t, done, info = env.step(a_t[0])
 
-            s_t1 = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY, ob.speedZ, ob.wheelSpinVel/100.0, ob.rpm))
-        
-            buff.add(s_t, a_t[0], r_t, s_t1, done)      #Add replay buffer
-            
-            #Do the batch update
+            s_t1 = ob
+
+            buff.add(s_t, a_t[0], r_t, s_t1, done)  # Add replay buffer
+
+            # Do the batch update
             batch = buff.getBatch(BATCH_SIZE)
             states = np.asarray([e[0] for e in batch])
             actions = np.asarray([e[1] for e in batch])
@@ -115,16 +93,16 @@ def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
             dones = np.asarray([e[4] for e in batch])
             y_t = np.asarray([e[1] for e in batch])
 
-            target_q_values = critic.target_model.predict([new_states, actor.target_model.predict(new_states)])  
-           
+            target_q_values = critic.target_model.predict([new_states, actor.target_model.predict(new_states)])
+
             for k in range(len(batch)):
                 if dones[k]:
                     y_t[k] = rewards[k]
                 else:
-                    y_t[k] = rewards[k] + GAMMA*target_q_values[k]
-       
+                    y_t[k] = rewards[k] + GAMMA * target_q_values[k]
+
             if (train_indicator):
-                loss += critic.model.train_on_batch([states,actions], y_t) 
+                loss += critic.model.train_on_batch([states, actions], y_t)
                 a_for_grad = actor.model.predict(states)
                 grads = critic.gradients(states, a_for_grad)
                 actor.train(states, grads)
@@ -133,9 +111,10 @@ def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
 
             total_reward += r_t
             s_t = s_t1
-        
-            print("Episode", i, "Step", step, "Action", a_t, "Reward", r_t, "Loss", loss)
-        
+
+            if debug:
+                print("Episode", i, "Step", step, "Action", a_t, "Reward", r_t, "Loss", loss)
+
             step += 1
             if done:
                 break
@@ -151,12 +130,18 @@ def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
                 with open("criticmodel.json", "w") as outfile:
                     json.dump(critic.model.to_json(), outfile)
 
-        print("TOTAL REWARD @ " + str(i) +"-th Episode  : Reward " + str(total_reward))
+        print("TOTAL REWARD @ " + str(i) + "-th Episode  : Reward " + str(total_reward))
         print("Total Step: " + str(step))
         print("")
+        if train_indicator == 1:
+            log.write('{}\n'.format(total_reward))
+            log.flush()
 
-    env.end()  # This is for shutting down TORCS
+    env.close()
+    if train_indicator == 1:
+        log.close()
     print("Finish.")
 
+
 if __name__ == "__main__":
-    playGame()
+    playGame(train_indicator=1, render=True, debug=False)
